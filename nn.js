1854b9a4f716643cb52b48f08eccdfaa95e280db
Ahtisham <Ahtisham.kashif@arkhitech.com>
2020-12-31 16:15:38 +0500

    New Chanes

--- nn.js
+++ nn.js
@@ -3,6 +3,7 @@ describe('Test Cases for Foodnerd', () => {
   it('TC 1, Visits the  FoodNerd Website', () => {
     cy.visit('https://www.foodnerd.pk/')
     cy.url().should('eq', 'https://www.foodnerd.pk/')  //getting into that particular page
+    cy.wait(1000);
   })
  
   //Second test case for Clicking Locations
@@ -10,13 +11,13 @@ describe('Test Cases for Foodnerd', () => {
   it('TC 2,Clicking Locations',() => {
   cy.contains("Locations").click();//To click on locations and
   cy.url().should('eq', 'https://www.foodnerd.pk/locations')  //getting into that particular page
-  cy.wait();
+  cy.wait(1000);
 
 })
 it('TC 3,Searching for new Locations',() => {
-  cy.get('#search-field').type('islamabad resturant');
-  cy.get('//a[contains(text(),'Search')]').click();
+  cy.get('#search-field').type('KFC');
+  cy.get(//a[@class='btn clr-dark white-text']').click();
   //cy.url().should('eq', 'https://www.foodnerd.pk/locations')
-
+  cy.wait(1000);
 })
 })
